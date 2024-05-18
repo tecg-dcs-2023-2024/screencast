e981de0d7202d5240e54bb9b4531cbf3593dd33e
Dominique Vilain <dominiquevilain@users.noreply.github.com>
2024-05-18 20:03:38 +0200

    add icons and handle no-data cases

--- resources/views/contacts/index.view.php
+++ resources/views/contacts/index.view.php
@@ -17,22 +17,32 @@
     <?php
     partials('common_html_start');
     ?>
-    <h1 class="font-bold text-2xl">Contacts</h1>
+    <h1 class="font-bold text-2xl">Vos contacts</h1>
     <?php
-    component('forms.search', ['label' => 'Nom du contact']); ?>
-
-    <section>
-        <h2 class="font-bold">Mes contacts</h2>
+    if (!empty($contacts)) : ?>
         <?php
-        component('contacts.list', [
-            'title' => 'Mes Contacts',
-            'contacts' => $contacts,
-        ]) ?>
-    </section>
+        component('forms.search', ['label' => 'Nom du contact']); ?>
 
+        <section>
+            <h2 class="font-bold">Mes contacts</h2>
+            <?php
+            component('contacts.list', [
+                'title' => 'Mes Contacts',
+                'contacts' => $contacts,
+            ]) ?>
+        </section>
+    <?php
+    endif ?>
     <div>
         <a href="/contact/create"
-           class="underline text-blue-500">Créer un nouveau contact</a>
+           class="underline text-blue-500 flex items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 24 24"
+                 fill="currentColor"
+                 class="w-6 h-6">
+                <path d="M5.25 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM2.25 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM18.75 7.5a.75.75 0 0 0-1.5 0v2.25H15a.75.75 0 0 0 0 1.5h2.25v2.25a.75.75 0 0 0 1.5 0v-2.25H21a.75.75 0 0 0 0-1.5h-2.25V7.5Z" />
+            </svg>
+            <span>Créer un nouveau contact</span></a>
     </div>
     <?php
     partials('common_html_end');
--- resources/views/jiris/index.view.php
+++ resources/views/jiris/index.view.php
@@ -20,31 +20,50 @@
     <?php
     partials('common_html_start');
     ?>
-    <h1 class="font-bold text-2xl">Jiris</h1>
+    <h1 class="font-bold text-2xl">Vos jiris</h1>
     <?php
-    component('forms.search', ['label' => 'Nom du jiri']); ?>
-
-    <section>
-        <h2 class="font-bold">Jiris à venir</h2>
+    if (!empty($past_jiris) || !empty($upcoming_jiris)) : ?>
         <?php
-        component('jiris.list', [
-            'title' => 'Jiris à venir',
-            'jiris' => $upcoming_jiris,
-        ]) ?>
-    </section>
-
-    <section>
-        <h2 class="font-bold">Jiris passés</h2>
+        component('forms.search', ['label' => 'Nom du jiri']); ?>
         <?php
-        component('jiris.list', [
-            'title' => 'Jiris passés',
-            'jiris' => $past_jiris,
-        ]) ?>
-    </section>
-
+        if (!empty($upcoming_jiris)) : ?>
+            <section>
+                <h2 class="font-bold">Jiris à venir</h2>
+                <?php
+                component('jiris.list', [
+                    'title' => 'Jiris à venir',
+                    'jiris' => $upcoming_jiris,
+                ]) ?>
+            </section>
+        <?php
+        endif ?>
+        <?php
+        if (!empty($past_jiris)) : ?>
+            <section>
+                <h2 class="font-bold">Jiris passés</h2>
+                <?php
+                component('jiris.list', [
+                    'title' => 'Jiris passés',
+                    'jiris' => $past_jiris,
+                ]) ?>
+            </section>
+        <?php
+        endif ?>
+    <?php
+    endif ?>
     <div>
         <a href="/jiri/create"
-           class="underline text-blue-500">Créer un nouveau jiri</a>
+           class="underline text-blue-500 flex items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 viewBox="0 0 24 24"
+                 fill="currentColor"
+                 class="w-6 h-6">
+                <path fill-rule="evenodd"
+                      d="M5.625 1.5H9a3.75 3.75 0 0 1 3.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 0 1 3.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 0 1-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875ZM12.75 12a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V18a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V12Z"
+                      clip-rule="evenodd" />
+                <path d="M14.25 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 16.5 7.5h-1.875a.375.375 0 0 1-.375-.375V5.25Z" />
+            </svg>
+            <span>Créer un nouveau jiri</span></a>
     </div>
     <?php
     partials('common_html_end');
